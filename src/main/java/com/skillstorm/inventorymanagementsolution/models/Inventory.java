0e23eb13444fe086501f0722a6cf3e5e1a58d9d8
h-huynh <henryh1404@gmail.com>
2023-07-06 16:19:36 -0500

    Inventory.java and InventoryId.java
    completed setup for Inventory and InventoryId

--- src/main/java/com/skillstorm/inventorymanagementsolution/models/Inventory.java
+++ src/main/java/com/skillstorm/inventorymanagementsolution/models/Inventory.java
@@ -1,5 +1,114 @@
 package com.skillstorm.inventorymanagementsolution.models;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "INVENTORY")
 public class Inventory {
-    
+
+    @EmbeddedId
+    private InventoryId id;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public Inventory() {
+    }
+
+    public Inventory(InventoryId id, Warehouse warehouse, Item item, int quantity) {
+        this.id = id;
+        this.warehouse = warehouse;
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public InventoryId getId() {
+        return id;
+    }
+
+    public void setId(InventoryId id) {
+        this.id = id;
+    }
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((warehouse == null) ? 0 : warehouse.hashCode());
+        result = prime * result + ((item == null) ? 0 : item.hashCode());
+        result = prime * result + quantity;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Inventory other = (Inventory) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (warehouse == null) {
+            if (other.warehouse != null)
+                return false;
+        } else if (!warehouse.equals(other.warehouse))
+            return false;
+        if (item == null) {
+            if (other.item != null)
+                return false;
+        } else if (!item.equals(other.item))
+            return false;
+        if (quantity != other.quantity)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Inventory [id=" + id + ", warehouse=" + warehouse + ", item=" + item + ", quantity=" + quantity + "]";
+    }
 }
--- src/main/java/com/skillstorm/inventorymanagementsolution/models/InventoryId.java
+++ src/main/java/com/skillstorm/inventorymanagementsolution/models/InventoryId.java
@@ -1,5 +1,63 @@
 package com.skillstorm.inventorymanagementsolution.models;
 
-public class InventoryId {
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class InventoryId implements Serializable {
+    private int warehouse;
+    private int item;
     
+    public InventoryId(int warehouse, int item) {
+        this.warehouse = warehouse;
+        this.item = item;
+    }
+
+    public int getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(int warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    public int getItem() {
+        return item;
+    }
+
+    public void setItem(int item) {
+        this.item = item;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + warehouse;
+        result = prime * result + item;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        InventoryId other = (InventoryId) obj;
+        if (warehouse != other.warehouse)
+            return false;
+        if (item != other.item)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryId [warehouse=" + warehouse + ", item=" + item + "]";
+    }
+
 }
