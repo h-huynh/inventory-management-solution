0356400871a184ed4e69ab70ddbd51c292e1dd97
h-huynh <henryh1404@gmail.com>
2023-07-07 17:10:43 -0500

    Update ItemService.java
    add CRUD service functionality to ItemService.java

--- src/main/java/com/skillstorm/inventorymanagementsolution/services/ItemService.java
+++ src/main/java/com/skillstorm/inventorymanagementsolution/services/ItemService.java
@@ -1,7 +1,41 @@
 package com.skillstorm.inventorymanagementsolution.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillstorm.inventorymanagementsolution.models.Item;
 import com.skillstorm.inventorymanagementsolution.repositories.ItemRepository;
 
+@Service
 public class ItemService {
+
+    @Autowired
     ItemRepository itemRepository;
+
+    // CREATE/UPDATE Items
+    public Item saveItem(Item Item) {
+        return itemRepository.save(Item);
+    }
+
+    // READ Items
+    public List<Item> findAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findItemById(int id) {
+        Optional<Item> item = itemRepository.findById(id);
+        if(item.isPresent()) {        
+            return item.get();         
+        }
+        return null;
+    }
+
+    // DELETE Items
+    public void deleteItem(Item item) {
+        itemRepository.delete(item);
+    }
+
 }
