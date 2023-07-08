8163a9f0c3ed8231e63e7ed308efeedbe4212deb
h-huynh <henryh1404@gmail.com>
2023-07-08 16:00:32 -0500

    WarehouseController.java implementation

--- src/main/java/com/skillstorm/inventorymanagementsolution/controllers/WarehouseController.java
+++ src/main/java/com/skillstorm/inventorymanagementsolution/controllers/WarehouseController.java
@@ -1,5 +1,72 @@
 package com.skillstorm.inventorymanagementsolution.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skillstorm.inventorymanagementsolution.models.Warehouse;
+import com.skillstorm.inventorymanagementsolution.services.WarehouseService;
+
+@RestController
+@RequestMapping("/warehouses")
+@CrossOrigin
 public class WarehouseController {
-    
+    @Autowired
+    WarehouseService warehouseService;
+
+    @GetMapping
+     public ResponseEntity<List<Warehouse>> getAllWarehouses() {
+        List<Warehouse> warehouses = warehouseService.findAllWarehouses();
+        return new ResponseEntity<>(warehouses, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Warehouse> getWarehouseById(@PathVariable int id) {
+        Warehouse warehouse = warehouseService.findWarehouseById(id);
+        if (warehouse != null) {
+            return new ResponseEntity<>(warehouse, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Warehouse> createWarehouse(@RequestBody Warehouse warehouse) {
+        Warehouse createdWarehouse = warehouseService.saveWarehouse(warehouse);
+        return new ResponseEntity<>(createdWarehouse, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Warehouse> updateWarehouse(@PathVariable int id, @RequestBody Warehouse warehouse) {
+        Warehouse existingWarehouse = warehouseService.findWarehouseById(id);
+        if (existingWarehouse != null) {
+            warehouse.setId(id);
+            Warehouse updatedWarehouse = warehouseService.saveWarehouse(warehouse);
+            return new ResponseEntity<>(updatedWarehouse, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWarehouse(@PathVariable int id) {
+        Warehouse existingWarehouse = warehouseService.findWarehouseById(id);
+        if (existingWarehouse != null) {
+            warehouseService.deleteWarehouse(existingWarehouse);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
