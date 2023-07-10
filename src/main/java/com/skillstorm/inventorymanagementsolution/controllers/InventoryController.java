7cce42a11380a6d23213865d11bcc26b20abfb93
h-huynh <henryh1404@gmail.com>
2023-07-09 19:19:08 -0500

    Update InventoryController.java
    added an endpoint to get all entries by a certain warehouseId, an endpoint to get totalCapacity of a warehouse (number of items in storage) and removed broken capacity validations

--- src/main/java/com/skillstorm/inventorymanagementsolution/controllers/InventoryController.java
+++ src/main/java/com/skillstorm/inventorymanagementsolution/controllers/InventoryController.java
@@ -33,6 +33,16 @@ public class InventoryController {
         return new ResponseEntity<>(inventoryList, HttpStatus.OK);
     }
 
+    @GetMapping("/{warehouseId}")
+    public ResponseEntity<List<Inventory>> getInventoryByWarehouseId(@PathVariable int warehouseId) {
+        List<Inventory> inventoryList = inventoryService.findInventoryByWarehouseId(warehouseId);
+        if (inventoryList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return new ResponseEntity<>(inventoryList, HttpStatus.OK);
+        }
+    }
+
     @GetMapping("/{warehouseId}/{itemId}")
     public ResponseEntity<Inventory> getInventory(@PathVariable int warehouseId, @PathVariable int itemId) {
         Inventory inventory = inventoryService.findInventoryByWarehouseIdAndItemId(warehouseId, itemId);
@@ -43,38 +53,30 @@ public class InventoryController {
         }
     }
 
+    @GetMapping("/{warehouseId}/totalQuantity")
+    public ResponseEntity<?> getWarehouseTotalQuantity(@PathVariable int warehouseId) {
+        int totalQuantity = inventoryService.getTotalQuantityByWarehouseId(warehouseId);
+
+        return new ResponseEntity<>(totalQuantity, HttpStatus.OK);
+    }
+
     // Adds a new item entry and its associated warehouse to the inventory table.
     @PostMapping
     public ResponseEntity<?> createInventory(@RequestBody Inventory inventory) {
-        int warehouseId = inventory.getWarehouse().getId();
-        int totalQuantity = inventoryService.getTotalQuantityByWarehouseId(warehouseId);
-
-        if (totalQuantity + inventory.getQuantity() <= inventory.getWarehouse().getMaximumCapacity()) {
-            Inventory createdInventory = inventoryService.saveInventory(inventory);
-            return new ResponseEntity<>(createdInventory, HttpStatus.CREATED);
-        } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body("Total inventory quantity exceeds warehouse capacity");
-        }
+        Inventory createdInventory = inventoryService.saveInventory(inventory);
+        return new ResponseEntity<>(createdInventory, HttpStatus.CREATED);
     }
 
     @PutMapping("/{warehouseId}/{itemId}")
-        public ResponseEntity<?> updateInventory(@PathVariable int warehouseId, @PathVariable int itemId, @Valid @RequestBody Inventory inventory) {
-            Inventory existingInventory = inventoryService.findInventoryByWarehouseIdAndItemId(warehouseId, itemId);
-            if (existingInventory == null) {
-                return ResponseEntity.notFound().build();
-            }
-
-            int totalQuantity = inventoryService.getTotalQuantityByWarehouseId(warehouseId) - existingInventory.getQuantity();
-
-            if (totalQuantity + inventory.getQuantity() <= existingInventory.getWarehouse().getMaximumCapacity()) {
-                inventory.setInventoryId(existingInventory.getInventoryId());
-                Inventory updatedInventory = inventoryService.saveInventory(inventory);
-                return new ResponseEntity<>(updatedInventory, HttpStatus.OK);
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body("Total inventory quantity exceeds warehouse capacity");
+    public ResponseEntity<?> updateInventory(@PathVariable int warehouseId, @PathVariable int itemId, @Valid @RequestBody Inventory inventory) {
+        Inventory existingInventory = inventoryService.findInventoryByWarehouseIdAndItemId(warehouseId, itemId);
+        if (existingInventory == null) {
+            return ResponseEntity.notFound().build();
         }
+
+        inventory.setInventoryId(existingInventory.getInventoryId());
+        Inventory updatedInventory = inventoryService.saveInventory(inventory);
+        return new ResponseEntity<>(updatedInventory, HttpStatus.OK);
     }
 
     @DeleteMapping("/{warehouseId}/{itemId}")
