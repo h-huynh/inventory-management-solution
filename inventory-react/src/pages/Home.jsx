1e49a5f8d7131ff74f15e1c3fdfe1da9ff35b888
h-huynh <henryh1404@gmail.com>
2023-07-14 03:14:10 -0500

    Update Home.jsx
    added landing page with instructions

--- inventory-react/src/pages/Home.jsx
+++ inventory-react/src/pages/Home.jsx
@@ -1,9 +1,44 @@
+import { GridContainer, ProcessList, ProcessListHeading, ProcessListItem } from "@trussworks/react-uswds";
+
 export default function Home() {
     
     
     return (
         <>
-            
+            <GridContainer>
+                <ProcessList>
+                    <ProcessListItem>
+                        <ProcessListHeading type="h4">Manage Warehouses</ProcessListHeading>
+                        <p className="margin-top-05">
+                           Warehouses stores a list of all items with their respective ids, names, location, current storage, and maximum capacity.
+                           When creating new warehouses, maximum capacity must be set to a minimum of atleast 10. Use the IDs as a reference for
+                           managing inventory. 
+                        </p>
+                    </ProcessListItem>
+
+                    <ProcessListItem>
+                        <ProcessListHeading type="h4">Manage Items Catalog</ProcessListHeading>
+                        <p className="margin-top-05">
+                            The item catalog stores a list of all items with their respective ids and names. Use this as a reference sheet to manage inventory.
+                        </p>
+                    </ProcessListItem>
+
+                    <ProcessListItem>
+                        <ProcessListHeading type="h4">Manage Inventory</ProcessListHeading>
+                        <p className="margin-top-05">
+                            Inventory manages all warehouses and their stored items. It is sorted by item ID and displays the quantity of
+                            a given item at a given warehouse. 
+
+                            <br/>
+                            <br/>
+
+                            When creating new inventory objects, you must input a valid item ID and warehouse ID along with a quantity
+                            that will cause the current storage of a warehouse to exceed maximum capacity. Edited quantities must also
+                            not cause the capacity to be exceeded.
+                        </p>
+                    </ProcessListItem>
+                </ProcessList>
+            </GridContainer>
         </>
     );
 }
